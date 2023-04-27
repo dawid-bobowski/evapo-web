e7423800829e184fc7864910262252795e004592
Dawid Bobowski <dbobowski3@gmail.com>
2023-04-27 17:36:08 +0200

    Fix tooltip values

--- src/components/CustomTooltip.tsx
+++ src/components/CustomTooltip.tsx
@@ -1,4 +1,4 @@
-import { NameType, ValueType } from 'recharts/types/component/DefaultTooltipContent';
+import { NameType, Payload, ValueType } from 'recharts/types/component/DefaultTooltipContent';
 import { TooltipProps } from 'recharts';
 import { Box } from '@mui/material';
 
@@ -7,20 +7,24 @@ import _ from 'lodash';
 
 const CustomTooltip = ({ active, payload, label }: TooltipProps<ValueType, NameType>) => {
   if (active && payload && payload.length) {
+    const T: Payload<ValueType, NameType> | undefined = payload.find((field) => field.name === 'T');
+    const Et0: Payload<ValueType, NameType> | undefined = payload.find((field) => field.name === 'Et0');
     return (
       <Box className='custom-tooltip'>
         <p>
           <span style={{ fontWeight: 'bold' }}>Dzień: </span>
           {convertDate(label)}
         </p>
-        <p>
-          <span style={{ fontWeight: 'bold' }}>T: </span>
-          {payload[0].value}
-        </p>
-        {payload[1] && (
+        {T && (
+          <p>
+            <span style={{ fontWeight: 'bold' }}>T: </span>
+            {T.value}
+          </p>
+        )}
+        {Et0 && (
           <p>
             <span style={{ fontWeight: 'bold' }}>Et0: </span>
-            {_.round(payload[1].value as number)}
+            {_.round(Et0.value as number)}
           </p>
         )}
       </Box>
